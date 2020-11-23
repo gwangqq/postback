72b7ab682dd68bf311b3922d7fb386b49970db8f
gwangq <pgg961@gmail.com>
2020-11-23 17:54:53 +0900

    organic data test

---
+++ lookback_window_test.php
@@ -0,0 +1,30 @@
+<?php
+			$servername = "localhost";
+			$username = "jakeworks";
+			$password = "qkrrhkdrb1!";
+			$dbname = "jakeworks";
+
+
+			$is_organic = $_GET['is_organic'];
+			$event_date_time = $_GET['event_date_time'];
+			$is_attr_owner = $_GET['is_attr_owner'];
+			$adid = $_GET['adid'];
+			
+			// Create connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check connection
+			if ($conn->connect_error) {
+			  die("Connection failed: " . $conn->connect_error);
+			}
+
+			$sql = "INSERT INTO testDB3 (is_organic, event_date_time, is_attr_owner, adid)
+			VALUES ('$is_organic', '$event_date_time', '$is_attr_owner', '$adid')";
+
+			if ($conn->query($sql) === TRUE) {
+			  echo "New record created successfully";
+				echo "insert query successfully done!!!";
+			} else {
+			  echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+				$conn->close();
+			?>
