da304f3824b7629159a12b0db9325e28d411dcbe
gwangq <pgg961@gmail.com>
2021-02-04 17:10:13 +0900

    lookback window test done

--- lookback_window_test.php
+++ lookback_window_test.php
@@ -7,9 +7,14 @@
 
 			$is_organic = $_GET['is_organic'];
 			$event_date_time = $_GET['event_date_time'];
-			$is_attr_owner = $_GET['is_attr_owner'];
+			$is_attr_owner = $_GET['is_attr_owner_event'];
 			$adid = $_GET['adid'];
 			$campaign = $_GET['campaign'];
+			$attr_owner = $_GET['attr_owner'];
+			$attribution_name = $_GET['attribution_name'];
+			$in_app_event = $_GET['in_app_event'];
+
+
 			// Create connection
 			$conn = new mysqli($servername, $username, $password, $dbname);
 			// Check connection
@@ -17,8 +22,8 @@
 			  die("Connection failed: " . $conn->connect_error);
 			}
 
-			$sql = "INSERT INTO testDB3 (is_organic, event_date_time, is_attr_owner, adid, campaign)
-			VALUES ('$is_organic', '$event_date_time', '$is_attr_owner', '$adid', '$campaign')";
+			$sql = "INSERT INTO testDB3 (is_organic, event_date_time, is_attr_owner, adid, campaign, attr_owner, attribution_name, in_app_event)
+			VALUES ('$is_organic', '$event_date_time', '$is_attr_owner', '$adid', '$campaign', '$attr_owner', '$attribution_name', '$in_app_event')";
 
 			if ($conn->query($sql) === TRUE) {
 			  echo "New record created successfully";
